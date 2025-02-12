197db7fb97e620c5435bdff906526b2f9294e19b
snareren <54242308+snareren@users.noreply.github.com>
2025-02-12 09:02:45 +0800

    Update DC1.h

--- include/DC1.h
+++ include/DC1.h
@@ -79,7 +79,7 @@ public:
     void init();
     String getModuleName() { return F("dc1"); }
     String getModuleCNName() { return F("DC1插线板"); }
-    String getModuleVersion() { return F("2022.12.06.0001"); }
+    String getModuleVersion() { return F("2025.02.12.1000"); }
     String getModuleAuthor() { return F("情留メ蚊子"); }
     bool moduleLed();
 
